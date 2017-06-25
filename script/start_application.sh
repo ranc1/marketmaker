8722d09971e5b5a81437f056cf2595231338416e
ranc1 <ranchen92@gmail.com>
2017-06-25 11:25:22 +0000

    Correct start_application script

--- script/start_application.sh
+++ script/start_application.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 pkill screen
-screen -d -m -S marketmaker bash -c 'cd ~/workspace/marketmaker-codedeploy && python3 /home/ubuntu/workspace/marketmaker/main.py'
+screen -d -m -S marketmaker bash -c 'cd ~/workspace/marketmaker-codedeploy && python3 main.py'
